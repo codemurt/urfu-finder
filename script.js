d4e446db9cfa54fca530bda6eeb114a20c5bf185
codemurt <egor.lebe@inbox.ru>
2021-04-27 14:46:02 +0500

    Created opening and changed system of couting

--- script.js
+++ script.js
@@ -10,181 +10,181 @@ const DATA = [
 	{
 		question: '1. Легко знакомлюсь с людьми.',
 		Psychotype: Psychotypes.HumanHuman,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '2. Охотно и подолгу могу что-нибудь мастерить.',
 		Psychotype: Psychotypes.HumanTech,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '3. Люблю ходить в музеи, театры, на выставки.',
 		Psychotype: Psychotypes.HumanArt,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '4. Охотно и постоянно ухаживаю за растениями, животными.',
 		Psychotype: Psychotypes.HumanNature,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '5. Охотно и подолгу могу что-нибудь вычислять, чертить.',
 		Psychotype: Psychotypes.HumanSign,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '6. С удовольствием общаюсь со сверстниками или малышами.',
 		Psychotype: Psychotypes.HumanHuman,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '7. С удовольствием ухаживаю за растениями и животными.',
 		Psychotype: Psychotypes.HumanNature,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '8. Обычно делаю мало ошибок в письменных работах.',
 		Psychotype: Psychotypes.HumanSign,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '9. Мои изделия обычно вызывают интерес у товарищей, старших.',
 		Psychotype: Psychotypes.HumanTech,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '10. Люди считают, что у меня есть художественные способности.',
 		Psychotype: Psychotypes.HumanArt,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '11. Охотно читаю о растениях, животных.',
 		Psychotype: Psychotypes.HumanNature,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '12. Принимаю участие в спектаклях, концертах.',
 		Psychotype: Psychotypes.HumanArt,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '13. Люблю читать об устройстве механизмов, приборов, машин.',
 		Psychotype: Psychotypes.HumanTech,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '14. Подолгу могу разгадывать головоломки, задачи, ребусы.',
 		Psychotype: Psychotypes.HumanSign,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '15. Легко улаживаю разногласия между людьми.',
 		Psychotype: Psychotypes.HumanHuman,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '16. Считают, что у меня есть способности к работе с техникой.',
 		Psychotype: Psychotypes.HumanTech,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '17. Людям нравится мое художественное творчество.',
 		Psychotype: Psychotypes.HumanArt,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '18. У меня есть способности к работе с растениями и животными.',
 		Psychotype: Psychotypes.HumanNature,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '19. Я могу ясно излагать свои мысли в письменной форме.',
 		Psychotype: Psychotypes.HumanSign,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '20. Я почти никогда ни с кем не ссорюсь.',
 		Psychotype: Psychotypes.HumanHuman,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '21. Результаты моего технического творчества одобряют даже незнакомые люди.',
 		Psychotype: Psychotypes.HumanTech,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '22. Без особого труда усваиваю иностранные языки.',
 		Psychotype: Psychotypes.HumanSign,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '23. Мне часто случается помогать даже незнакомым людям',
 		Psychotype: Psychotypes.HumanHuman,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '24. Подолгу могу заниматься музыкой, рисованием, читать книги и т. д.',
 		Psychotype: Psychotypes.HumanArt,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '25. Могу влиять на ход развития растений и животных.',
 		Psychotype: Psychotypes.HumanNature,
-		value: 2
+		weightQuestion: 2
 	},
 
 	{
 		question: '26. Люблю разбираться в устройстве механизмов, приборов.',
 		Psychotype: Psychotypes.HumanTech,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '27. Мне обычно удается убедить людей в своей правоте.',
 		Psychotype: Psychotypes.HumanHuman,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '28. Охотно наблюдаю за растениями или животными.',
 		Psychotype: Psychotypes.HumanNature,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '29.Охотно читаю научно-популярную, критическую литературу, публицистику.',
 		Psychotype: Psychotypes.HumanSign,
-		value: 1
+		weightQuestion: 1
 	},
 
 	{
 		question: '30. Стараюсь понять секреты мастерства и пробую свои силы в живописи, музыке и т. п.',
 		Psychotype: Psychotypes.HumanArt,
-		value: 1
+		weightQuestion: 1
 	}
 ];
 
@@ -196,6 +196,8 @@ const results = document.getElementById('results');
 const indicator = document.getElementById('indicator');
 const btnBack = document.getElementById('btn-back');
 const btnRestart = document.getElementById('btn-restart');
+const btnStart = document.getElementById('btn-start');
+const opening = document.getElementById('opening');
 
 const checkAvailableBack = (index) => {
 	if (index == 0)
@@ -207,8 +209,9 @@ const checkAvailableBack = (index) => {
 const renderQuestions = (index) => {
 	const renderButtons = () => {
 		return `
-			<button class="button-input" name="${index}" value="yes" id="${DATA[index].Psychotype}">Да</button>
-			<button class="button-input" name="${index}" value="no" id="${DATA[index].Psychotype}">Нет</button>
+			<button class="button-input" name="${index}" value="yes" id="${DATA[index].Psychotype}_${DATA[index].weightQuestion}">Да</button>
+			<button class="button-input" name="${index}" value="no" id="${DATA[index].Psychotype}_${DATA[index].weightQuestion}">Нет</button>
+			<button class="button-input" name="${index}" value="nothing" id="${DATA[index].Psychotype}_${DATA[index].weightQuestion}">Затрудняюсь ответить</button>
 		`;
 	};
 
@@ -229,13 +232,13 @@ const renderQuestions = (index) => {
 const renderResults = () => {
 
 	const countPsychotype = (psychotype) => {
-		var cnt = 0;
+		var sum = 0;
 		for (key in localResults){
-			if (localResults[key] == psychotype)
-				cnt++;
+			if (localResults[key].resultPsychotype == psychotype)
+				sum += Number(localResults[key].resultWeight);
 		}
 
-		return cnt;
+		return sum;
 	};
 
 	results.innerHTML = `
@@ -255,16 +258,22 @@ const renderIndicator = (currentStep) => {
 
 quiz.addEventListener('click', (event) => {
 	if (event.target.classList.contains('button-input')){
+
+		var targetPsychotype = event.target.id.split("_")[0];
+		var targetWeight = event.target.id.split("_")[1];
+
 		if (event.target.value == 'yes'){
-			localResults[event.target.name] = event.target.id;
+			localResults[event.target.name] = {resultPsychotype: targetPsychotype, resultWeight: targetWeight};
+		} else if(event.target.value == 'no') {
+			localResults[event.target.name] = {resultPsychotype: targetPsychotype, resultWeight: -targetWeight};
 		} else {
-			localResults[event.target.name] = '';
+			localResults[event.target.name] = {resultPsychotype: targetPsychotype, resultWeight: 0};
 		}
 
 		const nextQuestionIndex = Number(questions.dataset.currentStep) + 1;
 
 		if (DATA.length == nextQuestionIndex){
-			questions.classList.add('questions-hidden');
+			questions.classList.add('questions--hidden');
 			results.classList.add('results--visible');
 			indicator.classList.add('indicator--hidden');
 			btnBack.classList.add('btn-back--hidden');
@@ -287,7 +296,7 @@ quiz.addEventListener('click', (event) => {
 		results.innerHTML = '';
 		localResults = {};
 
-		questions.classList.remove('questions-hidden');
+		questions.classList.remove('questions--hidden');
 		results.classList.remove('results--visible');
 		indicator.classList.remove('indicator--hidden');
 		btnBack.classList.remove('btn-back--hidden');
@@ -297,4 +306,14 @@ quiz.addEventListener('click', (event) => {
 	}
 });
 
-renderQuestions(0);
+opening.addEventListener('click', (event) => {
+	if (event.target.classList.contains('btn-start')){
+
+		opening.classList.add('opening--hidden');
+		btnBack.classList.remove('btn-back--hidden');
+
+		renderQuestions(0);
+}
+});
+
+btnBack.classList.add('btn-back--hidden');
