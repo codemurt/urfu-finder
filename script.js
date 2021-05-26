1fe3cf3d8dcd84c55ea7642f9421db2d8ba24c6e
codemurt <egor.lebe@inbox.ru>
2021-05-26 21:52:28 +0500

    Added description of psychotypes

--- script.js
+++ script.js
@@ -188,6 +188,13 @@ const DATA = [
 	}
 ];
 
+let PsychotypeDescription = new Map();
+PsychotypeDescription.set(Psychotypes.HumanNature, 'Представителей этих профессий объединяет одно очень важное качество — любовь к природе. Но любовь не созерцательная. Которой обладают практически все люди, считая природу наиболее благоприятной средой для отдыха, а деятельная связанная с познанием ее законов и применением их. Одно дело — любить животных и растения, играть с ними, радоваться им. И совсем другое — регулярно, день за днем ухаживать за ними, наблюдать, лечить, выгуливать, не считаясь с личным временем и планами. Специалист должен не просто все знать о живых организмах, но и прогнозировать возможные изменения в них и принимать меры. От человека требуется инициатива и самостоятельность в решении конкретных задач, заботливость, терпение и дальновидность. Человек, работающий в сфере «человек-природа», должен быть спокойным и уравновешенным');
+PsychotypeDescription.set(Psychotypes.HumanTech, 'Особенность технических объектов в том, что они, как правило, могут быть точно измерены по многим признакам. При их обработке, преобразовании, перемещении или оценке от работника требуется точность, определенность действий. Техника как предмет руда представляет широкие возможности для новаторства, выдумки. творчества, поэтому важное значение приобретает такое качество, как практическое мышление. Техническая фантазия, способность мысленно соединять и разъединять технические объекты и их части — важные условия для успеха в данной области.');
+PsychotypeDescription.set(Psychotypes.HumanSign, 'Мы встречаемся со знаками значительно чаще, чем обычно представляем себе. Это цифры. Коды, условные знаки, естественные или искусственные языки, чертежи, таблицы формулы. В любом случае человек воспринимает знак как символ реального объекта или явления. Поэтому специалисту, который работает со знаками, важно уметь с одной стороны, абстрагироваться от реальных физических, химически, механических свойств предметов, а с другой — представлять и воспринимать характеристики реальных явлений или объектов, стоящих за знаками. Чтобы успешно работать в какой-нибудь профессии данного типа, необходимо уметь мысленно погружаться в мир, казалось бы, сухих обозначений и сосредотачиваться на сведениях, которые они несут в себе. Особые требования профессии этого типа предъявляют к вниманию.');
+PsychotypeDescription.set(Psychotypes.HumanArt, 'Важнейшие требования, которые предъявляют профессии, связанные с изобразительной, музыкальной, литературно-художественной, актерско-сценической деятельностью человека — наличие способности к искусствам, творческое воображение, образное мышление, талант, трудолюбие.');
+PsychotypeDescription.set(Psychotypes.HumanHuman, 'Даже выбирая профессию, не связанную непосредственно с общением, вы поступите правильно, если обратите внимание на общительность и контактность. Подумайте, куда вы  обращены- к людям или к себе? С кем бы вы хотели общаться — с собой или с другими? Главное содержание труда в профессиях типа «человек-человек» сводится к взаимодействию между людьми. Если не наладится это взаимодействие, значит, не наладится и работа. Качества, необходимые для работы с людьми: устойчивое, хорошее настроение в процессе работы с людьми, потребность в общении, способность мысленно ставить себя на место другого человека, быстро понимать намерения, помыслы, настроение людей, умение разбираться в человеческих взаимоотношениях, хорошая память (умение держать в уме имена и особенности многих людей), умение находить общий язык с различными людьми.');
+
 let localResults = {};
 
 const quiz = document.getElementById('quiz');
@@ -241,36 +248,72 @@ const renderResults = () => {
 		return (sum + 8) / 16 * 100;
 	};
 
-	let percentHumanNature = countPsychotype(Psychotypes.HumanNature);
-	let percentHumanTech = countPsychotype(Psychotypes.HumanTech);
-	let percentHumanSign = countPsychotype(Psychotypes.HumanSign);
-	let percentHumanArt = countPsychotype(Psychotypes.HumanArt);
-	let percentHumanHuman = countPsychotype(Psychotypes.HumanHuman);
+	// TO DO
+	let percentHumanNature = {Count: countPsychotype(Psychotypes.HumanNature), Psycho: Psychotypes.HumanNature};
+	let percentHumanTech = {Count: countPsychotype(Psychotypes.HumanTech), Psycho: Psychotypes.HumanTech};
+	let percentHumanSign = {Count: countPsychotype(Psychotypes.HumanSign), Psycho: Psychotypes.HumanSign};
+	let percentHumanArt = {Count: countPsychotype(Psychotypes.HumanArt), Psycho: Psychotypes.HumanArt};
+	let percentHumanHuman = {Count: countPsychotype(Psychotypes.HumanHuman), Psycho: Psychotypes.HumanHuman};
 
-	results.innerHTML = `
-		<div class="quiz-results-item">
-			Человек-природа:</br>
-			<div class="progress">
-  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanNature}%;" aria-valuenow="${percentHumanNature}" aria-valuemin="0" aria-valuemax="100">${percentHumanNature}%</div>
-			</div>
-			Человек-техника:</br>
-			<div class="progress">
-  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanTech}%;" aria-valuenow="${percentHumanTech}" aria-valuemin="0" aria-valuemax="100">${percentHumanTech}%</div>
-			</div>
-			Человек-знаковая система:</br>
-			<div class="progress">
-  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanSign}%;" aria-valuenow="${percentHumanSign}" aria-valuemin="0" aria-valuemax="100">${percentHumanSign}%</div>
-			</div>
-			Челоевек-художественный образ:</br>
-			<div class="progress">
-  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanArt}%;" aria-valuenow="${percentHumanArt}" aria-valuemin="0" aria-valuemax="100">${percentHumanArt}%</div>
-			</div>
-			Человек-человек:</br>
-			<div class="progress">
-  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanHuman}%;" aria-valuenow="${percentHumanHuman}" aria-valuemin="0" aria-valuemax="100">${percentHumanHuman}%</div>
-			</div>
-  		</div>	
-	`;
+	let maxPercentPsychotype = Math.max(percentHumanNature.Count, percentHumanTech.Count, percentHumanSign.Count, percentHumanSign.Count,
+		percentHumanArt.Count, percentHumanHuman.Count);
+
+	let countMaxPercent = Number(percentHumanNature.Count == maxPercentPsychotype) + Number(percentHumanTech.Count == maxPercentPsychotype)
+		+ Number(percentHumanSign.Count == maxPercentPsychotype) + Number(percentHumanArt.Count == maxPercentPsychotype) +
+		Number(percentHumanHuman.Count == maxPercentPsychotype);
+
+	console.log(countMaxPercent);
+	console.log(maxPercentPsychotype)
+
+		results.innerHTML = `
+			<div class="quiz-results-item">
+				Человек-природа:</br>
+				<div class="progress">
+	  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanNature.Count}%;" aria-valuenow="${percentHumanNature.Count}" aria-valuemin="0" aria-valuemax="100">${percentHumanNature.Count}%</div>
+				</div>
+				Человек-техника:</br>
+				<div class="progress">
+	  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanTech.Count}%;" aria-valuenow="${percentHumanTech.Count}" aria-valuemin="0" aria-valuemax="100">${percentHumanTech.Count}%</div>
+				</div>
+				Человек-знаковая система:</br>
+				<div class="progress">
+	  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanSign.Count}%;" aria-valuenow="${percentHumanSign.Count}" aria-valuemin="0" aria-valuemax="100">${percentHumanSign.Count}%</div>
+				</div>
+				Челоевек-художественный образ:</br>
+				<div class="progress">
+	  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanArt.Count}%;" aria-valuenow="${percentHumanArt.Count}" aria-valuemin="0" aria-valuemax="100">${percentHumanArt.Count}%</div>
+				</div>
+				Человек-человек:</br>
+				<div class="progress">
+	  				<div class="progress-bar" role="progressbar" style="width: ${percentHumanHuman.Count}%;" aria-valuenow="${percentHumanHuman.Count}" aria-valuemin="0" aria-valuemax="100">${percentHumanHuman.Count}%</div>
+				</div>
+	  		</div>	
+		`;
+
+	if (countMaxPercent == 1){
+
+		let resultPsychotype;
+
+		if (percentHumanNature.Count == maxPercentPsychotype)
+			resultPsychotype = percentHumanNature.Psycho;
+		else if (percentHumanTech.Count == maxPercentPsychotype)
+			resultPsychotype = percentHumanTech.Psycho;
+		else if (percentHumanSign.Count == maxPercentPsychotype)
+			resultPsychotype = percentHumanSign.Psycho;
+		else if (percentHumanArt.Count == maxPercentPsychotype)
+			resultPsychotype = percentHumanArt.Psycho;
+		else 
+			resultPsychotype = percentHumanHuman.Psycho;
+
+		results.innerHTML += `<div class="quiz-results-item"> 
+			<h1> Ваш психотип: ${resultPsychotype} </h1>
+			<p>${PsychotypeDescription.get(resultPsychotype)} </p>
+		</div>`
+	} else {
+		results.innerHTML += `<div class="quiz-results-item"> 
+			<h3>Мы не смогли точно установить ваш психотип, поэтому пожалуйста пройдите тест заново.</h3>
+		</div>`
+	}
 };
 
 const renderIndicator = (currentStep) => {
